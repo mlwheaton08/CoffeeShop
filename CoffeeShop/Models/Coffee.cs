81bd39fccaf49620a141898e409f49908621a880
Mason Wheaton <mlwheaton06@gmail.com>
2023-03-06 21:20:54 -0600

    in the middle of part 2. need to create coffee controller

--- CoffeeShop/Models/Coffee.cs
+++ CoffeeShop/Models/Coffee.cs
@@ -5,11 +5,8 @@ namespace CoffeeShop.Models;
 
 public class Coffee
 {
-    private readonly BeanVarietyRepository _beanVarietyRepository = new BeanVarietyRepository();
-
     public int Id { get; set; }
     public string Style { get; set; }
     public int BeanVarietyId { get; set; }
-    public BeanVariety BeanVariety { get { return _beanVarietyRepository.Get(BeanVarietyId); } }
-    public double Value { get; set; }
+    public BeanVariety BeanVariety { get; set; }
 }
--- CoffeeShop/Models/Order.cs
+++ CoffeeShop/Models/Order.cs
@@ -1,28 +1,28 @@
-﻿using System.Drawing;
+﻿//using System.Drawing;
 
-namespace CoffeeShop.Models;
+//namespace CoffeeShop.Models;
 
-public class Order
-{
-    private Dictionary<string, double> _sizesValue = new Dictionary<string, double>()
-    {
-        { "Small", 1 },
-        { "Medium", 1.7 },
-        { "Large", 2.3 }
-    };
+//public class Order
+//{
+//    private Dictionary<string, double> _sizesValue = new Dictionary<string, double>()
+//    {
+//        { "Small", 1 },
+//        { "Medium", 1.7 },
+//        { "Large", 2.3 }
+//    };
 
-    public int Id { get; set; }
-    public string CustomerName { get; set; }
-    public Coffee Coffee { get; set; }
-    public string Size { get; set; }
-    public string Notes { get; set; }
-    public double Price
-    {
-        get
-        {
-            double sizePrice = _sizesValue.FirstOrDefault(x => x.Key == Size).Value;
-            return sizePrice * Coffee.Value;
-        }
-    }
-    public bool Completed { get; set; } = false;
-}
+//    public int Id { get; set; }
+//    public string CustomerName { get; set; }
+//    public Coffee Coffee { get; set; }
+//    public string Size { get; set; }
+//    public string Notes { get; set; }
+//    public double Price
+//    {
+//        get
+//        {
+//            double sizePrice = _sizesValue.FirstOrDefault(x => x.Key == Size).Value;
+//            return sizePrice * Coffee.Value;
+//        }
+//    }
+//    public bool Completed { get; set; } = false;
+//}
